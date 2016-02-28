571c19c4dbf3c205b51d8cda3b5f357993b8a5e4
BK Bolisetty <bk@squarefruitlabs.com>
2016-02-28 16:24:03 -0600

    BRB

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 	belongs_to :user
 	validates :title, presence: true, length: {minimum: 3}
 	validates :body, presence: true
-	validates :product_model, presence: true
+	#validates :product_model, presence: true
 	attachment :product_image
 	attachment :product_model
 	#has_many :product_image, dependent: :destroy

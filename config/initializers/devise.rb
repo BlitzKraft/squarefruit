2bebbf889545f22041498e6ffed355920868140b
BK Bolisetty <bk@squarefruitlabs.com>
2016-04-05 01:03:32 -0500

    Access to user profile is sufficient.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, '396a966ceef6d3270a06', '00df7ef96239f2e2a4700b8034412ba87cb59e6e', scope: 'user,public_repo'
+  config.omniauth :github, '396a966ceef6d3270a06', '00df7ef96239f2e2a4700b8034412ba87cb59e6e', scope: 'user'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

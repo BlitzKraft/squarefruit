e89cf61ea4f6ded460da781321ee20f4f735c4ff
BK Bolisetty <bk@squarefruitlabs.com>
2016-04-13 04:23:43 -0500

    Added registrations controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :products
   resources :charges
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" } 
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: "registrations" } 
   root to: "products#index"
   post "/charges/new", to: "charges#create"
+  
 end
